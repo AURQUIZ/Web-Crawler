635c8ef72ea3ba4977023b6ff01e435c3af943cf
Antwan Love <askyourwife@10-4-221-70.vlan869.client.wireless.uic.edu>
2018-02-08 15:04:04 -0600

    updated. Version works

--- demo.py
+++ demo.py
@@ -5,11 +5,21 @@ class GoogleSpider(scrapy.Spider):
     start_urls = ['https://www.google.com/search?q=sql+injections+tutorial&oq=sql+injections+tutorial&aqs=chrome..69i57j0l5.641j0j4&sourceid=chrome&ie=UTF-8']
 
     def parse(self, response):
-        SET_SELECTOR = '.set'
-        for brickset in response.css(SET_SELECTOR):
-            NAME_SELECTOR = 'h1 a ::text'
+        SET_SELECTOR = '.g'
+       	for brickset in response.css(SET_SELECTOR):
+            
+            NAME_SELECTOR = 'h3 a ::text'
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract_first(),
             }
 
+        for i in range(10):
+            NEXT_PAGE_SELECTOR = '.b.navend a ::attr(href)'
+            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+            if next_page:
+        
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parse
+                )
 
