68bf85280bf1715b102fe772c6c8bc5bbc3a3745
eleon23 <32808558+eleon23@users.noreply.github.com>
2018-03-14 21:31:37 -0500

    Update webCrawler.py

--- webCrawler.py
+++ webCrawler.py
@@ -5,7 +5,6 @@ query  = "How to make a sql query with user input from {0}".format(sys.argv[1])
 response = GoogleSearch().search(query, num_results = 100)
 newfile = "{0}.txt".format(sys.argv[1])
 fd = open(newfile, 'a')
-count = 0;
 for result in response.results:
 	fd.write(result.url + '\n')
 fd.close()
